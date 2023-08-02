1de23e3b366e33f92577f3dd070921ed1f87c891
Aiswarya-AS <aiswaryaasubash@gmail.com>
2023-08-02 12:51:08 +0530

    added alembic to project

--- backend/db/base_class.py
+++ backend/db/base_class.py
@@ -1,9 +1,9 @@
-from tying import Any
+from typing import Any
 from sqlalchemy.ext.declarative import declared_attr
 from sqlalchemy.orm import as_declarative
 
 
-@as_declarative
+@as_declarative()
 class Base:
     id:Any
     __name__:str
